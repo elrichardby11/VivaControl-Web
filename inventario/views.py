647150829291819226a6bdf1e4e1d1f8750df766
elrichardby11 <richardmazuelos@gmail.com>
2024-07-06 02:40:01 -0400

    inventario works

--- inventario/views.py
+++ inventario/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import CStateMovimiento, DetalleMovimiento, Movimiento, TipoMovimiento
+
+from administrador.models import Sucursal
+from .models import CStateMovimiento, DetalleMovimiento, Movimiento, SucursalProducto, TipoMovimiento
 from django.core.paginator import Paginator             
 
 @login_required
@@ -51,34 +53,27 @@ def inventory_list(request):
 
     # Capturando los parámetros de busqueda
     search_query = request.GET.get('search', '')
-    tipo_query = request.GET.get('tipo', '')
-    estado_query = request.GET.get('estado', '')
+    local_query = request.GET.get('local', '')
 
-    movimientos = Movimiento.objects.all()
+    inventarios = SucursalProducto.objects.all()
 
     if search_query:
-        movimientos = movimientos.filter(auxiliar__rut_auxiliar__contains = search_query)
+        inventarios = inventarios.filter(id_producto__id_producto__contains = search_query)
 
-    if tipo_query:
-        movimientos = movimientos.filter(tipo_movimiento = tipo_query)
+    if local_query:
+        inventarios = inventarios.filter(id_sucursal = local_query)
 
-    if estado_query:
-        movimientos = movimientos.filter(cstate_movimiento = estado_query)
-    
-    paginator = Paginator(movimientos, 10)  # Mostrar 5 elementos por página
+    paginator = Paginator(inventarios, 10)  # Mostrar 5 elementos por página
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    tipos = TipoMovimiento.objects.all()
-    estados = CStateMovimiento.objects.all()
+    locales = Sucursal.objects.all()
 
     context = {
         'page_obj': page_obj,
-        'tipos': tipos,
-        'estados': estados,
+        'locales': locales,
         'search_query': search_query,
-        'tipo_query': tipo_query,
-        'estado_query': estado_query,
+        'local_query': local_query,
     }
 
     return render(request, 'inventario.html', context)
